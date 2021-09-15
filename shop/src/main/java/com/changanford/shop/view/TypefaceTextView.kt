5c5d41fe4b2492f378dbf5d39afa3d272e6b1460
wen <15730095669@163.com>
2021-09-15 17:43:24 +0800

    shop

---
+++ shop/src/main/java/com/changanford/shop/view/TypefaceTextView.kt
@@ -0,0 +1,38 @@
+package com.changanford.shop.view
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.changanford.shop.R
+
+
+/**
+ * @Author : wenke
+ * @Time : 2021/9/15
+ * @Description : TypefaceTextView
+ */
+class TypefaceTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    AppCompatTextView(context, attrs, defStyleAttr) {
+    init {
+        initTypefaceTextView(context, attrs)
+        initView()
+    }
+    private fun initTypefaceTextView(context: Context, attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TypefaceTextView)
+        //字体
+        val typefaceName = typedArray.getString(R.styleable.TypefaceTextView_typeface) ?: return
+        val typeface: Typeface= when (typefaceName) {
+            "ZenDots-Regular" -> Typeface.createFromAsset(context.assets, "$typefaceName.ttf")
+            else ->Typeface.DEFAULT
+        }
+        setTypeface(typeface)
+        //行高
+        setLineSpacing(0f,1.5f)
+        typedArray.recycle()
+    }
+    private fun initView(){
+
+    }
+
+}
