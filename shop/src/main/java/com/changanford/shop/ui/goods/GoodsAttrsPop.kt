a333d05624222e87276575ecbbf9bdaa4678f37d
wen <15730095669@163.com>
2021-09-22 14:46:57 +0800

    商城-商品详情-商品属性选择

--- shop/src/main/java/com/changanford/shop/ui/goods/GoodsAttrsPop.kt
+++ shop/src/main/java/com/changanford/shop/ui/goods/GoodsAttrsPop.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.view.animation.Animation
 import androidx.databinding.DataBindingUtil
 import com.changanford.shop.R
+import com.changanford.shop.adapter.goods.GoodsAttributeAdapter
 import com.changanford.shop.databinding.PopGoodsSelectattributeBinding
 import razerdp.basepopup.BasePopupWindow
 import razerdp.util.animation.AnimationHelper
 import razerdp.util.animation.TranslationConfig
-
 /**
  * @Author : wenke
  * @Time : 2021/9/22
@@ -16,13 +16,23 @@ import razerdp.util.animation.TranslationConfig
  */
 open class GoodsAttrsPop(context: Context?): BasePopupWindow(context) {
     private var viewDataBinding: PopGoodsSelectattributeBinding = DataBindingUtil.bind(createPopupById(R.layout.pop_goods_selectattribute))!!
+    private val colorAdapter by lazy { GoodsAttributeAdapter(0) }
+    private val specificationAdapter by lazy { GoodsAttributeAdapter(0) }
     init {
         contentView=viewDataBinding.root
         initView()
+        initData()
     }
     private fun initView(){
         viewDataBinding.imgClose.setOnClickListener { this.dismiss() }
+        viewDataBinding.rvColor.adapter=colorAdapter
+        viewDataBinding.rvSpecifications.adapter=specificationAdapter
     }
+    private fun initData(){
+        colorAdapter.setList(arrayListOf("红色","黑色","蓝色"))
+        specificationAdapter.setList(arrayListOf("64G","128G","512G"))
+    }
+    //动画
     override fun onCreateShowAnimation(): Animation? {
         return AnimationHelper.asAnimation()
             .withTranslation(TranslationConfig.FROM_BOTTOM)
