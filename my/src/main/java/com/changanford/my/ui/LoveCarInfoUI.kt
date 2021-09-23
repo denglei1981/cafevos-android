6326e87ea477a9384ea03f9dff97ea5df9178e27
zhouchunyu <15123166707@163.com>
2021-09-23 09:28:30 +0800

    22

--- my/src/main/java/com/changanford/my/ui/LoveCarInfoUI.kt
+++ my/src/main/java/com/changanford/my/ui/LoveCarInfoUI.kt
@@ -1,10 +1,22 @@
 package com.changanford.my.ui
 
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.changanford.common.basic.EmptyViewModel
+import com.changanford.common.router.path.ARouterMyPath
+import com.changanford.my.BaseMineUI
+import com.changanford.my.databinding.UiLoveCarInfoBinding
+
 /**
  *  文件名：LoveCarInfoUI
  *  创建者: zcy
  *  创建日期：2021/9/22 17:40
  *  描述: TODO
  *  修改描述：TODO
- */class LoveCarInfoUI {
+ */
+@Route(path = ARouterMyPath.MineLoveCarInfoUI)
+class LoveCarInfoUI : BaseMineUI<UiLoveCarInfoBinding, EmptyViewModel>() {
+
+    override fun initView() {
+
+    }
 }
