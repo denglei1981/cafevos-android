cba61a8e0e3cb3049b3d4ff6464b7818e77c4c5a
wen <15730095669@163.com>
2021-09-23 15:53:07 +0800

    商城-积分明细

--- shop/src/main/java/com/changanford/shop/ui/IntegralDetailsActivity.kt
+++ shop/src/main/java/com/changanford/shop/ui/IntegralDetailsActivity.kt
@@ -1,7 +1,10 @@
 package com.changanford.shop.ui
 
+import android.content.Context
+import android.content.Intent
 import com.changanford.common.basic.BaseActivity
 import com.changanford.shop.ShopViewModel
+import com.changanford.shop.adapter.IntegralDetailsAdapter
 import com.changanford.shop.databinding.ActIntegralDetailsBinding
 
 /**
@@ -10,8 +13,14 @@ import com.changanford.shop.databinding.ActIntegralDetailsBinding
  * @Description : 积分明细
  */
 class IntegralDetailsActivity:BaseActivity<ActIntegralDetailsBinding,ShopViewModel> (){
+    companion object{
+        fun start(context: Context) {
+            context.startActivity(Intent(context, IntegralDetailsActivity::class.java))
+        }
+    }
+    private val mAdapter by lazy { IntegralDetailsAdapter() }
     override fun initView() {
-
+        binding.recyclerView.adapter=mAdapter
     }
 
     override fun initData() {
--- shop/src/main/java/com/changanford/shop/view/TopBar.kt
+++ shop/src/main/java/com/changanford/shop/view/TopBar.kt
@@ -1,10 +1,11 @@
 package com.changanford.shop.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.changanford.shop.R
 
 /**
@@ -12,18 +13,21 @@ import com.changanford.shop.R
  * @Time : 2021/9/9
  * @Description : TitleBar
  */
-class TitleBar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-    LinearLayout(context, attrs, defStyleAttr), View.OnClickListener {
+class TopBar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+    ConstraintLayout(context, attrs), View.OnClickListener {
 
 
     init {
+        LayoutInflater.from(context).inflate(R.layout.view_topbar, this)
         initAttributes(context, attrs)
-        LayoutInflater.from(context).inflate(R.layout.in_header_layout, this)
         initView()
     }
+    @SuppressLint("CustomViewStyleable", "Recycle")
     private fun initAttributes(context: Context, attrs: AttributeSet?) {
         //获取自定义属性
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.TitleBar)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TopBar)
+        val bgColor=typedArray.getResourceId(R.styleable.TopBar_bg_color,R.color.white)
+        setBackgroundResource(bgColor)
     }
 
     private fun initView() {
