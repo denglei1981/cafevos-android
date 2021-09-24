b0ba818ad30621f2718d1a37d2a692eff66aa25e
wen <15730095669@163.com>
2021-09-24 17:00:24 +0800

    商城-商品订单-订单列表

--- shop/src/main/java/com/changanford/shop/adapter/order/OrderGoodsAdapter.kt
+++ shop/src/main/java/com/changanford/shop/adapter/order/OrderGoodsAdapter.kt
@@ -7,6 +7,7 @@ import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
 import com.changanford.shop.R
 import com.changanford.shop.bean.OrderBean
 import com.changanford.shop.databinding.ItemOrdersGoodsBinding
+import com.changanford.shop.popupwindow.PublicPop
 
 
 class OrderGoodsAdapter: BaseQuickAdapter<OrderBean, BaseDataBindingHolder<ItemOrdersGoodsBinding>>(R.layout.item_orders_goods), LoadMoreModule {
@@ -16,6 +17,18 @@ class OrderGoodsAdapter: BaseQuickAdapter<OrderBean, BaseDataBindingHolder<ItemO
         if(dataBinding!=null){
             dataBinding.model=item
             dataBinding.executePendingBindings()
+            //取消订单
+            dataBinding.tvBtnCancel.setOnClickListener {
+                val pop=PublicPop(context)
+                pop.showPopupWindow(context.getString(R.string.prompt_cancelOrder),null,null,object :PublicPop.OnPopClickListener{
+                    override fun onLeftClick() {
+                        pop.dismiss()
+                    }
+                    override fun onRightClick() {
+                        pop.dismiss()
+                    }
+                })
+            }
         }
     }
 }
