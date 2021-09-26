68c278d5f554d8214af470f7c056217a134db306
18723498864 <910839387@qq.com>
2021-09-26 15:59:00 +0800

    搜索帖子相关。

--- home/src/main/java/com/changanford/home/search/fragment/SearchNewsFragment.kt
+++ home/src/main/java/com/changanford/home/search/fragment/SearchNewsFragment.kt
@@ -1,11 +1,20 @@
 package com.changanford.home.search.fragment
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.changanford.common.basic.BaseFragment
 import com.changanford.common.basic.EmptyViewModel
 import com.changanford.home.databinding.HomeBaseRecyclerViewBinding
+import com.changanford.home.search.adapter.SearchPostResultAdapter
+import com.changanford.home.search.data.SearchData
 
 class SearchNewsFragment : BaseFragment<HomeBaseRecyclerViewBinding, EmptyViewModel>() {
+
+
+    var  shopLists = mutableListOf<SearchData>()
+    val searchPostResultAdapter : SearchPostResultAdapter by lazy {
+        SearchPostResultAdapter(mutableListOf())
+    }
 
     companion object {
         fun newInstance(): SearchNewsFragment {
@@ -17,7 +26,14 @@ class SearchNewsFragment : BaseFragment<HomeBaseRecyclerViewBinding, EmptyViewMo
     }
 
     override fun initView() {
-
+        binding.recyclerView.layoutManager=
+            LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL,false)
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        binding.recyclerView.adapter= SearchPostResultAdapter(shopLists)
     }
 
     override fun initData() {
--- home/src/main/java/com/changanford/home/search/fragment/SearchPostFragment.kt
+++ home/src/main/java/com/changanford/home/search/fragment/SearchPostFragment.kt
@@ -1,11 +1,20 @@
 package com.changanford.home.search.fragment
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.changanford.common.basic.BaseFragment
 import com.changanford.common.basic.EmptyViewModel
 import com.changanford.home.databinding.HomeBaseRecyclerViewBinding
+import com.changanford.home.search.adapter.SearchPostResultAdapter
+import com.changanford.home.search.adapter.SearchShopResultAdapter
+import com.changanford.home.search.data.SearchData
 
 class SearchPostFragment : BaseFragment<HomeBaseRecyclerViewBinding, EmptyViewModel>() {
+
+    var  shopLists = mutableListOf<SearchData>()
+    val searchPostResultAdapter : SearchPostResultAdapter by lazy {
+        SearchPostResultAdapter(mutableListOf())
+    }
 
 
     companion object {
@@ -18,7 +27,14 @@ class SearchPostFragment : BaseFragment<HomeBaseRecyclerViewBinding, EmptyViewMo
     }
 
     override fun initView() {
-
+        binding.recyclerView.layoutManager=
+            LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL,false)
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        shopLists.add(SearchData())
+        binding.recyclerView.adapter=SearchPostResultAdapter(shopLists)
     }
 
     override fun initData() {
