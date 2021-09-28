b6bf1b20cced92443d13f0c0614bd9c6f81775e9
dhaslkfha <974314011@qq.com>
2021-09-28 17:11:27 +0800

    福特地址

--- common/src/main/java/com/changanford/common/util/MConstant.kt
+++ common/src/main/java/com/changanford/common/util/MConstant.kt
@@ -12,8 +12,8 @@ import java.io.File
  * *********************************************************************************
  */
 object MConstant {
-    const val BASE_URL = "https://csapi.uniplanet.cn"
-//    const val BASE_URL = "https://evosapiqa.changanford.cn"
+//    const val BASE_URL = "https://csapi.uniplanet.cn"
+    const val BASE_URL = "https://evosapiqa.changanford.cn"
     const val isDebug = true
     const val LOGIN_TOKEN = "LOGIN_TOKEN"
     const val APP_MD5_KEY = "J5i6UkJi8voBEEyE1g5q"
