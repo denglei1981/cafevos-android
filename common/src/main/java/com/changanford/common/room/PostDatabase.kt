4ca16b996e7af4395049ef31360ca9ba883d360b
zy <zhouyi1003>
2021-10-11 20:39:53 +0800

    ....

--- common/src/main/java/com/changanford/common/room/PostDatabase.kt
+++ common/src/main/java/com/changanford/common/room/PostDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [PostEntity::class], version = 8, exportSchema = false)
+@Database(entities = [PostEntity::class], version = 9, exportSchema = false)
 abstract class PostDatabase : RoomDatabase(){
 
 
