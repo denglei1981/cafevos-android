cd1a5c0d0707252715b4e9f607f5e46756f1177e
18723498864 <910839387@qq.com>
2021-10-14 16:09:36 +0800

    首页二楼 广告。。。

--- home/src/main/java/com/changanford/home/news/fragment/NewsListFragment.kt
+++ home/src/main/java/com/changanford/home/news/fragment/NewsListFragment.kt
@@ -18,6 +18,7 @@ import com.changanford.common.util.bus.LiveDataBusKey.NEWS_DETAIL_CHANGE
 import com.changanford.common.util.toast.ToastUtils
 import com.changanford.common.utilext.toastShow
 import com.changanford.home.HomeV2Fragment
+import com.changanford.home.PageConstant
 import com.changanford.home.R
 import com.changanford.home.bean.SpecialListBean
 import com.changanford.home.data.InfoDetailsChangeData
@@ -165,8 +166,10 @@ class NewsListFragment : BaseLoadSirFragment<FragmentNewsListBinding, FindNewsLi
                     newsListAdapter.setNewInstance(dataList)
                     binding.smartLayout.finishRefresh()
                 }
-                if (dataList.size < it.data.pageSize) { // 没有请求的多, 不加载更多。
-                    binding.smartLayout.finishLoadMoreWithNoMoreData()
+                if (it.data.dataList.size < PageConstant.DEFAULT_PAGE_SIZE_THIRTY) {
+                    binding.smartLayout.setEnableLoadMore(false)
+                } else {
+                    binding.smartLayout.setEnableLoadMore(true)
                 }
             } else {
                 showFailure(it.message)
