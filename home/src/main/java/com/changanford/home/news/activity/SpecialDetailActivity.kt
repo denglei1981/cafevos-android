7842af0926f9d9c83f39665302040f8b40304e57
18723498864 <910839387@qq.com>
2021-10-19 09:41:21 +0800

    修复专题列表的显示。

--- home/src/main/java/com/changanford/home/news/activity/SpecialDetailActivity.kt
+++ home/src/main/java/com/changanford/home/news/activity/SpecialDetailActivity.kt
@@ -27,6 +27,7 @@ import com.changanford.home.news.adapter.NewsListAdapter
 import com.changanford.home.news.request.SpecialDetailViewModel
 import com.google.android.material.appbar.AppBarLayout
 import com.google.gson.Gson
+import com.luck.picture.lib.tools.ScreenUtils
 import jp.wasabeef.glide.transformations.BlurTransformation
 
 @Route(path = ARouterHomePath.SpecialDetailActivity)
@@ -78,7 +79,20 @@ class SpecialDetailActivity :
             viewModel.getSpecialDetail(topicId)
             setLoadSir(binding.recyclerView)
         }
+        setExpandView()
     }
+
+
+  fun setExpandView(){
+      val width: Int = ScreenUtils.getScreenWidth(this)
+      binding.layoutCollBar.tvTopic.initWidth(width)
+      binding.layoutCollBar.tvTopic.setMaxLines(3)
+      binding.layoutCollBar.tvTopic.setHasAnimation(true)
+      binding.layoutCollBar.tvTopic.setCloseInNewLine(true)
+      binding.layoutCollBar.tvTopic.setOpenSuffixColor(resources.getColor(R.color.blue_tab))
+      binding.layoutCollBar.tvTopic.setCloseSuffixColor(resources.getColor(R.color.blue_tab))
+
+  }
 
     override fun observe() {
         super.observe()
