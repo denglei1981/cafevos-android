ffcaf725550ed05ffc6f2c5d0a53c28c4ba76e02
18723498864 <910839387@qq.com>
2021-10-29 13:29:54 +0800

    资讯展示子评论。

--- home/src/main/java/com/changanford/home/bean/CommentListBean.kt
+++ home/src/main/java/com/changanford/home/bean/CommentListBean.kt
@@ -1,6 +1,7 @@
 package com.changanford.home.bean
 
 import com.changanford.common.bean.Imag
+import com.changanford.common.util.CountUtils
 import java.io.Serializable
 
 /**
@@ -9,32 +10,37 @@ import java.io.Serializable
  * @Des:
  */
 data class CommentListBean(
-    val avatar: String="",
-    var childCount: Int=0,
-    val content: String="",
-    val createTime: String="",
-    val groupId: String="",
-    val id: String="",
-    var isLike: Int=0,
-    val headFrameImage: String="",
-    var likesCount: Int=0,
-    val nickname: String="",
-    val phoneModel: String="",
-    val role: String="",
-    val timeStr: String="",
-    val userId: String="",
-    val memberIcon: String="",
+    val avatar: String = "",
+    var childCount: Int = 0,
+    val content: String = "",
+    val createTime: String = "",
+    val groupId: String = "",
+    val id: String = "",
+    var isLike: Int = 0,
+    val headFrameImage: String = "",
+    var likesCount: Int = 0,
+    val nickname: String = "",
+    val phoneModel: String = "",
+    val role: String = "",
+    val timeStr: String = "",
+    val userId: String = "",
+    val memberIcon: String = "",
     val imags: List<Imag> = arrayListOf(),
     val parentVo: List<CommentParentVo> = arrayListOf(),
     var isOpenParent: Boolean = false,
-    var typeNull:Int=0 // 没有数据1
+    var typeNull: Int = 0 // 没有数据1
 ) : Serializable {
     override fun toString(): String {
         return "CommentListBean(avatar='$avatar', childCount=$childCount, content='$content', createTime='$createTime', groupId='$groupId', id='$id', isLike=$isLike, likesCount=$likesCount, nickname='$nickname', phoneModel='$phoneModel', role='$role', timeStr='$timeStr', userId=$userId)"
     }
-
-
+    fun getChildCounts(): String {
+        if (childCount == 0) {
+            return "回复"
+        }
+        return CountUtils.formatNum(childCount.toString(), false).toString().plus("回复")
+    }
 }
+
 data class CommentParentVo(
     val avatar: String,
     val content: String,
