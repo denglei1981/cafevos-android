9e1076c9c85e8623ec064ab0058632726be91e8d
zy <zhouyi1003>
2021-10-29 11:02:01 +0800

    發帖參數問題

--- circle/src/main/java/com/changanford/circle/ui/activity/LongPostAvtivity.kt
+++ circle/src/main/java/com/changanford/circle/ui/activity/LongPostAvtivity.kt
@@ -283,10 +283,10 @@ class LongPostAvtivity : BaseActivity<LongpostactivityBinding, PostViewModule>()
         }
 
         if (isTopPost) {
-            params["topId"] = intent.extras?.getString("topId") ?: "0"
-            params["topName"] = intent.extras?.getString("topName") ?: ""
-            (params["topName"] as String).isNotEmpty().let {
-                buttomTypeAdapter.setData(2, ButtomTypeBean(params["topName"] as String, 1, 2))
+            params["topicId"] = intent.extras?.getString("topId") ?: "0"
+            params["topicName"] = intent.extras?.getString("topName") ?: ""
+            (params["topicName"] as String).isNotEmpty().let {
+                buttomTypeAdapter.setData(2, ButtomTypeBean(params["topicName"] as String, 1, 2))
             }
         }
     }
@@ -550,8 +550,8 @@ class LongPostAvtivity : BaseActivity<LongpostactivityBinding, PostViewModule>()
                 )
                 when(buttomTypeAdapter.getItem(position).itemType){
                     2 ->{
-                        params.remove("topID")
-                        params.remove("topName")
+                        params.remove("topicId")
+                        params.remove("topicName")
                     }
                     3->{
                         params.remove("circleId")
