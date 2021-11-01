550b6fe264c6dc6cd2203a2cc1ae2af27223a9a7
zy <zhouyi1003>
2021-11-01 18:23:44 +0800

    ...启动页

--- app/src/main/java/com/changanford/evos/SplashActivity.kt
+++ app/src/main/java/com/changanford/evos/SplashActivity.kt
@@ -1,21 +1,28 @@
 package com.changanford.evos
 
+import android.annotation.SuppressLint
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.changanford.common.basic.BaseActivity
 import com.changanford.common.basic.EmptyViewModel
 import com.changanford.common.router.path.ARouterHomePath
 import com.changanford.evos.databinding.ActivitySplashBinding
+import com.changanford.my.BaseMineUI
 import com.gyf.immersionbar.ImmersionBar
 
 @Route(path = ARouterHomePath.SplashActivity)
-class SplashActivity : BaseActivity<ActivitySplashBinding, EmptyViewModel>() {
+class SplashActivity : BaseMineUI<ActivitySplashBinding, EmptyViewModel>() {
+
     override fun initView() {
-        makeStateBarTransparent(true)
-        ImmersionBar.with(this).statusBarDarkFont(isDarkFont).init()
+//        makeStateBarTransparent(true)
+//        ImmersionBar.with(this).statusBarDarkFont(isDarkFont).init()
 
     }
 
     override fun initData() {
 
+    }
+
+    override fun isUseFullScreenMode(): Boolean {
+        return true
     }
 }
