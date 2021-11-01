071c2d3813e71fc3317e80ae9d6f292d96abf432
wen <15730095669@163.com>
2021-11-01 11:35:09 +0800

    商城

--- shop/src/main/java/com/changanford/shop/ui/order/PayConfirmActivity.kt
+++ shop/src/main/java/com/changanford/shop/ui/order/PayConfirmActivity.kt
@@ -68,6 +68,7 @@ class PayConfirmActivity:BaseActivity<ShopActPayconfirmBinding, OrderViewModel>(
         binding.tvAccountPoints.setHtmlTxt(getString(R.string.str_Xfb,dataBean?.acountFb),"#00095B")
         val payCountDown=dataBean?.waitPayCountDown?:waitPayCountDown
         if(payCountDown>0){
+            timeCountControl?.cancel()
             timeCountControl=PayTimeCountControl(payCountDown*1000,binding.tvPayTime,object : OnTimeCountListener {
                 override fun onFinish() {
                     payResults(false)
