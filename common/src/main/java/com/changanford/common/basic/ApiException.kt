d248a425867c7440d2069fd8555082adbed5d9cc
18723498864 <910839387@qq.com>
2021-11-04 20:39:47 +0800

    优化活动轮播

--- common/src/main/java/com/changanford/common/basic/ApiException.kt
+++ common/src/main/java/com/changanford/common/basic/ApiException.kt
@@ -3,7 +3,7 @@ package com.changanford.common.basic
 /**
  * 用来封装业务错误信息
  *
- * @author zs
- * @date 2020-05-09
+ * @author lw
+ * @date 2021-11-04
  */
 class ApiException(val errorMessage: String, val errorCode: Int) : Throwable()
