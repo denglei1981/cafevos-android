1808857b47fd854c35952304e538c4145098233e
18723498864 <910839387@qq.com>
2021-11-17 17:17:06 +0800

    搜索支持口令搜索。

--- home/src/main/java/com/changanford/home/search/fragment/SearchShopFragment.kt
+++ home/src/main/java/com/changanford/home/search/fragment/SearchShopFragment.kt
@@ -63,21 +63,15 @@ class SearchShopFragment: BaseLoadSirFragment<HomeBaseRecyclerViewBinding, PolyS
                 val item = searchShopResultAdapter.getItem(position)
                 selectPosition = position
                 // todo 跳转到商品
-
-                if(item.jumpDataType==null||item.jumpDataType == "0"){
-                    JumpUtils.instans!!.jump(3,item.mallMallSpuId)
-                }else{
-                    JumpUtils.instans!!.jump(
-                        item.jumpDataType.toInt(),
-                        item.jumpDataValue
-                    )
-                }
-
-
-
-
-
-
+                JumpUtils.instans!!.jump(3,item.mallMallSpuId)
+//                if(item.jumpDataType==null||item.jumpDataType == "0"){
+//                    JumpUtils.instans!!.jump(3,item.mallMallSpuId)
+//                }else{
+//                    JumpUtils.instans!!.jump(
+//                        item.jumpDataType.toInt(),
+//                        item.jumpDataValue
+//                    )
+//                }
 
             }
         })
