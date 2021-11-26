6dab9b8c3ba87958b94057cc1d063960579accd9
wen <15730095669@163.com>
2021-11-26 14:02:45 +0800

    商城-修改订单列表图片填充模式

--- shop/src/main/java/com/changanford/shop/control/OrderControl.kt
+++ shop/src/main/java/com/changanford/shop/control/OrderControl.kt
@@ -37,7 +37,7 @@ class OrderControl(val context: Context,val viewModel: OrderViewModel?) {
             if("2"==item.busSourse&&!TextUtils.isEmpty(item.hagglePrice)){
                 item.fbOfUnitPrice=item.hagglePrice!!
             }
-            imgGoodsCover.scaleType= if(item.orderType>2)ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.CENTER_INSIDE
+            imgGoodsCover.scaleType= if(item.orderType>2) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.CENTER_INSIDE
             imgGoodsCover.load(item.skuImg)
             tvOrderType.apply {
                 visibility = when(item.busSourse) {
