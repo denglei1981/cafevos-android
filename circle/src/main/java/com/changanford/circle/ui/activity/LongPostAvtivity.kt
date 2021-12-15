a6bc7e471ce64ed30e463c56e1524a05a8fc8824
zy <zhouyi1003>
2021-12-15 16:10:14 +0800

    当获取不到图片宽高处理

--- circle/src/main/java/com/changanford/circle/ui/activity/LongPostAvtivity.kt
+++ circle/src/main/java/com/changanford/circle/ui/activity/LongPostAvtivity.kt
@@ -961,9 +961,9 @@ class LongPostAvtivity : BaseActivity<LongpostactivityBinding, PostViewModule>()
                 stsBean.tempFilePath + System.currentTimeMillis() + "androidios${
                     if (media.isCut) {
                         if (rotation == ExifInterface.ORIENTATION_ROTATE_90 || rotation == ExifInterface.ORIENTATION_ROTATE_270) {
-                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
+                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 500)
                         } else {
-                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0);
+                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 500);
                         }
                     } else {
                         if (media.width == 0) {
@@ -975,9 +975,9 @@ class LongPostAvtivity : BaseActivity<LongpostactivityBinding, PostViewModule>()
                 }_${
                     if (media.isCut) {
                         if (rotation == ExifInterface.ORIENTATION_ROTATE_90 || rotation == ExifInterface.ORIENTATION_ROTATE_270) {
-                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0);
+                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 500);
                         } else {
-                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
+                            exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 500)
                         }
                     } else {
                         if (media.height == 0) {
