4820f681d96b1d96d8d968826964aa0b8cc94fbd
wen <15730095669@163.com>
2022-01-05 09:51:06 +0800

    社区-支持compose

---
+++ circle/src/main/java/com/changanford/circle/viewmodel/circle/NewCircleViewModel.kt
@@ -0,0 +1,11 @@
+package com.changanford.circle.viewmodel.circle
+
+import com.changanford.common.basic.BaseViewModel
+
+/**
+ * @Author : wenke
+ * @Time : 2022/1/5 0005
+ * @Description : CircleViewModel
+ */
+class NewCircleViewModel:BaseViewModel() {
+}
