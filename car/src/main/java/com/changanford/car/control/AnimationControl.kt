cfcefe4b5047516903193a7aab532716e7662024
wen <15730095669@163.com>
2022-01-19 11:17:57 +0800

    爱车-banner加动画

--- car/src/main/java/com/changanford/car/control/AnimationControl.kt
+++ car/src/main/java/com/changanford/car/control/AnimationControl.kt
@@ -5,35 +5,60 @@ import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationSet
 import android.view.animation.ScaleAnimation
 import android.view.animation.TranslateAnimation
+
 /**
  * @Author : wenke
  * @Time : 2022/1/18 0018
  * @Description : CarControl
  */
 class AnimationControl {
-    private val durationTime=1500L
+    private val durationTime=800L
     /**
      * [module]1向下 2向上 3向左 4向右
     * */
     fun startAnimation(view: View,module:Int?=1){
-        val animationSet = AnimationSet(true) //共用动画补间
-        animationSet.duration = durationTime
+        val animationSet = AnimationSet(true)
+//        animationSet.duration = durationTime
+        val fromYDelta=if(2==module)60f else -60f
+        val toYDelta=if(2==module)10f else -10f
         //位移
-        TranslateAnimation(0f, 0f, -60f, 0f).apply {
+        TranslateAnimation(0f, 0f, fromYDelta, 0f).apply {
             duration = durationTime
             repeatCount = 0 //动画的反复次数
-            fillAfter = true //设置为true，动画转化结束后被应用
+            fillAfter = false //设置为true，动画转化结束后被应用
             animationSet.addAnimation(this)
         }
         //透明
-        AlphaAnimation(0.5f, 1f).apply {
-            duration = durationTime //持续时间
+        AlphaAnimation(0.2f, 1f).apply {
+            duration = durationTime+300
             fillAfter=false
             animationSet.addAnimation(this)
         }
-        //缩放  x轴0倍，x轴1倍，y轴0倍，y轴1倍
-        ScaleAnimation(1f, 1f, 0.9f, 1f).apply {
+//        //缩放  x轴0倍，x轴1倍，y轴0倍，y轴1倍
+        ScaleAnimation(1f, 1f, 0.98f, 1f).apply {
+            duration = durationTime+300
+            animationSet.addAnimation(this)
+        }
+        view.startAnimation(animationSet)
+//        Handler(Looper.myLooper()!!).postDelayed({
+//            startAnimation0(view,module)
+//        },durationTime)
+    }
+   private fun startAnimation0(view: View,module:Int?=1){
+        val animationSet = AnimationSet(true) //共用动画补间
+        animationSet.duration = durationTime
+        val fromYDelta=if(2==module)10f else -10f
+        //位移
+        TranslateAnimation(0f, 0f, fromYDelta, 0f).apply {
             duration = durationTime
+            repeatCount = 0 //动画的反复次数
+            fillAfter = false //设置为true，动画转化结束后被应用
+            animationSet.addAnimation(this)
+        }
+//        //透明
+        AlphaAnimation(0.8f, 1f).apply {
+            duration = durationTime
+            fillAfter=false
             animationSet.addAnimation(this)
         }
         view.startAnimation(animationSet)
