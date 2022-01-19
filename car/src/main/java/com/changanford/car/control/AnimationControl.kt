559764351de0c657270d24e3e4de63770e2304e3
wen <15730095669@163.com>
2022-01-19 11:27:44 +0800

    爱车-banner加动画

--- car/src/main/java/com/changanford/car/control/AnimationControl.kt
+++ car/src/main/java/com/changanford/car/control/AnimationControl.kt
@@ -3,7 +3,6 @@ package com.changanford.car.control
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationSet
-import android.view.animation.ScaleAnimation
 import android.view.animation.TranslateAnimation
 
 /**
@@ -19,10 +18,33 @@ class AnimationControl {
     fun startAnimation(view: View,module:Int?=1){
         val animationSet = AnimationSet(true)
 //        animationSet.duration = durationTime
-        val fromYDelta=if(2==module)60f else -60f
+        var fromYDelta=0f
+        var fromXDelta=0f
         val toYDelta=if(2==module)10f else -10f
+        when(module){
+            1->{
+                fromYDelta=-60f
+                fromXDelta=0f
+            }
+            2->{
+                fromYDelta=60f
+                fromXDelta=0f
+            }
+            3->{
+                fromYDelta=0f
+                fromXDelta=-60f
+            }
+            4->{
+                fromYDelta=0f
+                fromXDelta=60f
+            }
+        }
+        //不使用动画
+        if(fromYDelta==0f&&fromXDelta==0f){
+            return
+        }
         //位移
-        TranslateAnimation(0f, 0f, fromYDelta, 0f).apply {
+        TranslateAnimation(fromXDelta, 0f, fromYDelta, 0f).apply {
             duration = durationTime
             repeatCount = 0 //动画的反复次数
             fillAfter = false //设置为true，动画转化结束后被应用
@@ -35,10 +57,10 @@ class AnimationControl {
             animationSet.addAnimation(this)
         }
 //        //缩放  x轴0倍，x轴1倍，y轴0倍，y轴1倍
-        ScaleAnimation(1f, 1f, 0.98f, 1f).apply {
-            duration = durationTime+300
-            animationSet.addAnimation(this)
-        }
+//        ScaleAnimation(1f, 1f, 0.98f, 1f).apply {
+//            duration = durationTime+300
+//            animationSet.addAnimation(this)
+//        }
         view.startAnimation(animationSet)
 //        Handler(Looper.myLooper()!!).postDelayed({
 //            startAnimation0(view,module)
