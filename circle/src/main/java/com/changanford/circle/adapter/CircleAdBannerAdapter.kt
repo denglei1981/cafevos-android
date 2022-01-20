97d88cbc506259227d5f2777715e6027d9f6b7a8
wen <15730095669@163.com>
2022-01-20 11:14:56 +0800

    合并108

--- circle/src/main/java/com/changanford/circle/adapter/CircleAdBannerAdapter.kt
+++ circle/src/main/java/com/changanford/circle/adapter/CircleAdBannerAdapter.kt
@@ -1,48 +1,46 @@
 package com.changanford.circle.adapter
 
-import android.view.View
+
 import androidx.databinding.DataBindingUtil
 import com.changanford.circle.R
 import com.changanford.circle.databinding.ItemCircleRecommendAdBinding
-import com.changanford.circle.databinding.ItemCircleRecommendOneBinding
 import com.changanford.common.bean.AdBean
 import com.changanford.common.util.JumpUtils
 import com.changanford.common.utilext.GlideUtils
-
-
 import com.zhpan.bannerview.BaseBannerAdapter
 import com.zhpan.bannerview.BaseViewHolder
 
-class CircleAdBannerAdapter : BaseBannerAdapter<AdBean, RecommendBarBannerViewHolder>() {
+class CircleAdBannerAdapter : BaseBannerAdapter<AdBean>() {
 
     override fun getLayoutId(viewType: Int): Int {
         return R.layout.item_circle_recommend_ad
     }
 
-
-    override fun createViewHolder(itemView: View?, viewType: Int): RecommendBarBannerViewHolder {
-        return RecommendBarBannerViewHolder(itemView!!)
-    }
-
-    override fun onBind(
-        holder: RecommendBarBannerViewHolder?,
+    override fun bindData(
+        holder: BaseViewHolder<AdBean>,
         data: AdBean,
         position: Int,
         pageSize: Int
     ) {
-        holder!!.bindData(data, position, pageSize)
-    }
 
-
-}
-
-class RecommendBarBannerViewHolder(itemView: View) : BaseViewHolder<AdBean>(itemView) {
-    override fun bindData(data: AdBean, position: Int, pageSize: Int) {
-        val binding = DataBindingUtil.bind<ItemCircleRecommendAdBinding>(itemView)
+        val binding = DataBindingUtil.bind<ItemCircleRecommendAdBinding>(holder.itemView)
 //        binding?.ivBanner?.load(data.adImg)
         GlideUtils.loadRound(data.adImg,binding?.ivBanner!!)
         binding.ivBanner.setOnClickListener {
             JumpUtils.instans?.jump(data.jumpDataType, data.jumpDataValue)
         }
     }
+
+
 }
+//
+//class RecommendBarBannerViewHolder(itemView: View) : BaseViewHolder<AdBean>(itemView) {
+//    override fun bindData(data: AdBean, position: Int, pageSize: Int) {
+//        val binding = DataBindingUtil.bind<ItemCircleRecommendAdBinding>(itemView)
+////        binding?.ivBanner?.load(data.adImg)
+//        GlideUtils.loadRound(data.adImg,binding?.ivBanner!!)
+//        binding.ivBanner.setOnClickListener {
+//            JumpUtils.instans?.jump(data.jumpDataType, data.jumpDataValue)
+//        }
+//    }
+//}
