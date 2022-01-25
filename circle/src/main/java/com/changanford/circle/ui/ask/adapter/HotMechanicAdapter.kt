19ef78045b330562b14b2570211c49cd42237d01
18723498864 <910839387@qq.com>
2022-01-25 17:13:05 +0800

    修复标签选中状态bug.

--- circle/src/main/java/com/changanford/circle/ui/ask/adapter/HotMechanicAdapter.kt
+++ circle/src/main/java/com/changanford/circle/ui/ask/adapter/HotMechanicAdapter.kt
@@ -1,22 +1,12 @@
 package com.changanford.circle.ui.ask.adapter
 
-import android.os.Bundle
-import android.os.UserManager
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.changanford.circle.R
 import com.changanford.circle.bean.CircleMemberBean
-import com.changanford.circle.databinding.ItemPersonalBinding
-import com.changanford.circle.ext.ImageOptions
-import com.changanford.circle.ext.loadImage
-import com.changanford.circle.utils.MUtils
-import com.changanford.common.manger.UserManger
-import com.changanford.common.router.path.ARouterMyPath
-import com.changanford.common.router.startARouter
-import com.changanford.common.util.MConstant
+import com.changanford.circle.databinding.ItemHotMechanicBinding
 
 /**
  *Author lcw
@@ -29,7 +19,7 @@ class HotMechanicAdapter :
 
 
     override fun convert(holder: BaseViewHolder, item: CircleMemberBean) {
-        val binding = DataBindingUtil.bind<ItemPersonalBinding>(holder.itemView)
+        val binding = DataBindingUtil.bind<ItemHotMechanicBinding>(holder.itemView)
         binding?.let {
 
         }
