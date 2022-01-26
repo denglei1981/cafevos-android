649304fd42795c2d01277e7b3d268d95ca9e0717
18723498864 <910839387@qq.com>
2022-01-26 09:55:25 +0800

    问答标题

--- circle/src/main/java/com/changanford/circle/bean/TestBean.kt
+++ circle/src/main/java/com/changanford/circle/bean/TestBean.kt
@@ -1,8 +1,20 @@
 package com.changanford.circle.bean
 
+import com.chad.library.adapter.base.entity.MultiItemEntity
+
 data class TestBean( val avatar: String,
                      val id: String,
                      var isLike: Int,
                      val headFrameImage: String,
                 ) {
+}
+
+data class MultiBean( val avatar: String,
+                     val id: String,
+                     var isLike: Int,
+                     val headFrameImage: String,
+) : MultiItemEntity {
+    override var itemType: Int = 0
+        get() = 0
+
 }
