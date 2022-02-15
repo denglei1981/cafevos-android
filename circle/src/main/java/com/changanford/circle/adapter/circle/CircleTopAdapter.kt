01422dbf9f423b38a6a4eb22b9cf859c528bc735
wen <15730095669@163.com>
2022-02-15 13:15:02 +0800

    社区-圈子-热门排行榜：修改排行榜前三名样式

--- circle/src/main/java/com/changanford/circle/adapter/circle/CircleTopAdapter.kt
+++ circle/src/main/java/com/changanford/circle/adapter/circle/CircleTopAdapter.kt
@@ -1,6 +1,7 @@
 package com.changanford.circle.adapter.circle
 
 import android.annotation.SuppressLint
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
@@ -11,13 +12,22 @@ import com.changanford.common.utilext.load
 import com.changanford.common.wutil.FlowLayoutManager
 
 class CircleTopAdapter: BaseQuickAdapter<NewCircleBean, BaseDataBindingHolder<ItemCircleHotlistLayoutBinding>>(R.layout.item_circle_hotlist_layout){
+    private val rankingIcons= arrayListOf(R.drawable.icon_huati_one,R.drawable.icon_huati_two,R.drawable.icon_huati_three)
     @SuppressLint("SetTextI18n")
     override fun convert(holder: BaseDataBindingHolder<ItemCircleHotlistLayoutBinding>, item: NewCircleBean) {
         holder.dataBinding?.apply {
             val position=holder.absoluteAdapterPosition
             imgCover.load(item.pic)
             wtvRanking.apply {
-                text="${position+1}"
+                if(position<3){
+                    visibility= View.GONE
+                    imgRanking.setImageResource(rankingIcons[position])
+                    imgRanking.visibility=View.VISIBLE
+                }else{
+                    text="${position+1}"
+                    visibility=View.VISIBLE
+                    imgRanking.visibility=View.INVISIBLE
+                }
                 setTextColor(ContextCompat.getColor(context,if(position<3)R.color.color_FC5E42 else R.color.color_D1D2D7))
             }
             item.tags?.let {tags->
