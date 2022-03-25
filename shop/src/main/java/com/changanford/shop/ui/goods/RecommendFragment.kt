92a729ee4071caeeec5c34091820e7ce78bb391a
wen <15730095669@163.com>
2022-03-25 15:47:17 +0800

    商城-榜单

--- shop/src/main/java/com/changanford/shop/ui/goods/RecommendFragment.kt
+++ shop/src/main/java/com/changanford/shop/ui/goods/RecommendFragment.kt
@@ -2,8 +2,8 @@ package com.changanford.shop.ui.goods
 
 import android.os.Bundle
 import com.changanford.common.basic.BaseFragment
+import com.changanford.shop.adapter.goods.ShopRecommendListAdapter
 import com.changanford.shop.databinding.FragmentRecommendBinding
-import com.changanford.shop.ui.compose.RecommendListCompose
 import com.changanford.shop.viewmodel.GoodsViewModel
 
 /**
@@ -22,6 +22,7 @@ class RecommendFragment:BaseFragment<FragmentRecommendBinding,GoodsViewModel>() 
         }
     }
     private var kindId:String?=null
+    private val mAdapter by lazy { ShopRecommendListAdapter() }
     override fun initView() {
         binding.srl.apply {
             setEnableLoadMore(false)
@@ -29,16 +30,13 @@ class RecommendFragment:BaseFragment<FragmentRecommendBinding,GoodsViewModel>() 
                 viewModel.getRecommendList(kindId?:"0")
             }
         }
+        binding.recyclerView.adapter=mAdapter
     }
 
     override fun initData() {
         viewModel.GoodsListBean.observe(this){
-            binding.apply {
-                composeView.setContent {
-                    RecommendListCompose(dataBean = it)
-                }
-                srl.finishRefresh()
-            }
+            mAdapter.setList(it)
+            binding.srl.finishRefresh()
         }
         arguments?.getString("kindId","0")?.apply {
             kindId=this
