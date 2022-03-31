9f7e7d014943fbd943f977877c4305d75f5f5c86
18723498864 <910839387@qq.com>
2022-03-31 21:00:11 +0800

    优惠券可用，不可用 ，失效。

--- shop/src/main/java/com/changanford/shop/ui/coupon/CouponActivity.kt
+++ shop/src/main/java/com/changanford/shop/ui/coupon/CouponActivity.kt
@@ -1,6 +1,8 @@
 package com.changanford.shop.ui.coupon
 
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -39,21 +41,52 @@ class CouponActivity : BaseActivity<ActivityMyCouponBinding, BaseViewModel>() {
         }
         binding.layoutTop.tvRight.text = "说明"
         binding.layoutTop.tvRight.visibility = View.VISIBLE
-    }
+        binding.searchTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                selectTab(tab, true)
+            }
 
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                selectTab(tab, false)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+    private fun selectTab(tab: TabLayout.Tab, isSelect: Boolean) {
+        val mTabText = tab.customView?.findViewById<TextView>(R.id.tv_title)
+        if (isSelect) {
+            mTabText?.isSelected = true
+            mTabText?.setTextColor(ContextCompat.getColor(MyApp.mContext, R.color.black))
+            mTabText?.paint?.isFakeBoldText = true
+
+        } else {
+            mTabText?.setTextColor(ContextCompat.getColor(MyApp.mContext, R.color.gray_999999))
+
+            mTabText?.paint?.isFakeBoldText = false// 取消加粗
+
+        }
+    }
     private fun initTab() {
-//        for (i in 0 until binding.searchTab.tabCount) {
-//            //寻找到控件
-//            val view: View = LayoutInflater.from(MyApp.mContext).inflate(R.layout.tab_home, null)
-//            val mTabText = view.findViewById<TextView>(R.id.tv_title)
-//
-//            mTabText.text = titleList[i]
-//            //更改选中项样式
-//            //设置样式
-//            binding.searchTab.getTabAt(i)?.customView = view
-//        }
-    }
+        for (i in 0 until binding.searchTab.tabCount) {
+            //寻找到控件
+            val view: View = LayoutInflater.from(MyApp.mContext).inflate(R.layout.tab_coupon, null)
+            val mTabText = view.findViewById<TextView>(R.id.tv_title)
 
+            mTabText.text = titleList[i]
+            if (itemPunchWhat == i) {
+                mTabText.isSelected = true
+                mTabText.setTextColor(ContextCompat.getColor(MyApp.mContext, R.color.black))
+            } else {
+                mTabText.setTextColor(ContextCompat.getColor(MyApp.mContext, R.color.gray_999999))
+            }
+            //更改选中项样式
+            //设置样式
+            binding.searchTab.getTabAt(i)?.customView = view
+        }
+    }
+    var itemPunchWhat: Int = 0
     override fun initData() {
         fragmentList.add(couponCanUseFragment)
         fragmentList.add(couponUseOverFragment)
