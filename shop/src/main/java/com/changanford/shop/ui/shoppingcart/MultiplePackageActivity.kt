5bd8e648dd5b4f83871877fd77b69e2dbf25b234
18723498864 <910839387@qq.com>
2022-04-01 11:19:59 +0800

    多包裹物流。

--- shop/src/main/java/com/changanford/shop/ui/shoppingcart/MultiplePackageActivity.kt
+++ shop/src/main/java/com/changanford/shop/ui/shoppingcart/MultiplePackageActivity.kt
@@ -2,12 +2,14 @@ package com.changanford.shop.ui.shoppingcart
 
 import com.changanford.common.basic.BaseActivity
 import com.changanford.shop.databinding.ActivityShoppingCartBinding
+import com.changanford.shop.databinding.BaseRecyclerViewBinding
 import com.changanford.shop.ui.shoppingcart.request.ShoppingCartViewModel
 
 /**
  *  多包裹发货
  * */
-class MultiplePackageActivity : BaseActivity<ActivityShoppingCartBinding, ShoppingCartViewModel>() {
+class MultiplePackageActivity : BaseActivity<BaseRecyclerViewBinding, ShoppingCartViewModel>() {
+
     override fun initView() {
 
     }
