4f7c20bc46e246eb2722f4f7fdb4b3d447fbd6d2
18723498864 <910839387@qq.com>
2022-04-02 17:04:16 +0800

    订单详情。

--- common/src/main/java/com/changanford/common/util/CustomImageSpan.kt
+++ common/src/main/java/com/changanford/common/util/CustomImageSpan.kt
@@ -27,3 +27,26 @@ class CustomImageSpan(drawable: Drawable?) : ImageSpan(drawable!!) {
         canvas.restore()
     }
 }
+
+class CustomImageSpanV2(drawable: Drawable?) : ImageSpan(drawable!!) {
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        val fm = paint.fontMetricsInt
+        val drawable = drawable
+
+        val transY = (y + fm.descent + y + fm.ascent) / 2 - drawable.bounds.bottom / 2 + top+2
+        canvas.save()
+        canvas.translate(x, transY.toFloat())
+        drawable.draw(canvas)
+        canvas.restore()
+    }
+}
