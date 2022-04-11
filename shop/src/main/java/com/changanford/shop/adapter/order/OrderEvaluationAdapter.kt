1f2fda27b9060b93480c57a8fc0485a0a1693bec
wen <15730095669@163.com>
2022-04-11 13:31:37 +0800

    商城-订单-评价

--- shop/src/main/java/com/changanford/shop/adapter/order/OrderEvaluationAdapter.kt
+++ shop/src/main/java/com/changanford/shop/adapter/order/OrderEvaluationAdapter.kt
@@ -4,16 +4,31 @@ import android.annotation.SuppressLint
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
 import com.changanford.common.bean.OrderItemBean
+import com.changanford.common.utilext.load
 import com.changanford.shop.R
+import com.changanford.shop.bean.PostEvaluationBean
 import com.changanford.shop.databinding.ItemPostEvaluationBinding
+import com.changanford.shop.utils.WCommonUtil.onTextChanged
 
 
 class OrderEvaluationAdapter: BaseQuickAdapter<OrderItemBean, BaseDataBindingHolder<ItemPostEvaluationBinding>>(R.layout.item_post_evaluation){
+    private val postBean:ArrayList<PostEvaluationBean> = arrayListOf()
     @SuppressLint("SetTextI18n")
     override fun convert(holder: BaseDataBindingHolder<ItemPostEvaluationBinding>, item: OrderItemBean) {
         holder.dataBinding?.apply {
+            val position=holder.absoluteAdapterPosition
             model=item
             executePendingBindings()
+            imgGoodsCover.load(item.skuImg)
+            edtContent.onTextChanged {
+                initPostBean(this, item, position)
+            }
+            initPostBean(this, item, position)
+        }
+    }
+    private fun initPostBean(dataBinding:ItemPostEvaluationBinding,item: OrderItemBean,position:Int){
+        dataBinding.apply {
+
         }
     }
 }
--- shop/src/main/java/com/changanford/shop/bean/OrderFormState.kt
+++ shop/src/main/java/com/changanford/shop/bean/OrderFormState.kt
@@ -1,15 +1,21 @@
 package com.changanford.shop.bean
 
-/**
- * Data validation state of the login form.
- */
 data class OrderFormState(
     val content:String?=null,
     val contentLength:Int=0,
     val isDataValid: Boolean = false,
     val rating:Int=0
 )
-
+/**
+ * 发布评价
+* */
+data class PostEvaluationBean(
+    var anonymous: String?=null,//是否匿名
+    var evalScore: Int? = null,//评分(0-5)
+    var evalText: String? = null,//评价内容
+    var imgUrls: List<String>? = null,
+    var mallMallOrderSkuId:String?= null,//订单skuId
+)
 data class RefundBean(var orderNo:String,var payFb:String?,var payRmb:String?,var refundType:String){
 
 }
