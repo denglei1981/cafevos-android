f0937f340c27507593a256ae0db15683dcf1b574
ning <974314011@qq.com>
2022-08-11 09:56:16 +0800

    地址没有区时展示问题

--- my/src/main/java/com/changanford/my/ui/AddressListUI.kt
+++ my/src/main/java/com/changanford/my/ui/AddressListUI.kt
@@ -125,8 +125,9 @@ class AddressListUI : BaseMineUI<UiAddressListBinding, AddressViewModel>() {
         ) {
             holder.dataBinding?.let {
                 it.itemAddName.text = "${item.consignee}  ${item.phone}"
+                //广东东莞没有区
                 it.itemAddress.text =
-                    "${item.provinceName}${item.cityName}${item.districtName}${item.addressName}"
+                    "${item.provinceName}${item.cityName}${item.districtName?:""}${item.addressName}"
                 it.cbDef.isChecked = item.isDefault == 1
                 it.cbDef.setTextColor(Color.parseColor(if (item.isDefault == 1) "#01025C" else "#72747B"))
                 it.cbDef.setOnClickListener {
