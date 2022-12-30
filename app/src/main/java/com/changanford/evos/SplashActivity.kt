7d94a638db7232c3e8d800384ccd05cae69d88d2
牛啵啵 <>
2022-12-30 09:31:28 +0800

    解决app后台返回前台进入启动页问题

--- app/src/main/java/com/changanford/evos/SplashActivity.kt
+++ app/src/main/java/com/changanford/evos/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.changanford.evos
 
+import android.content.Intent
+import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.changanford.common.basic.EmptyViewModel
 import com.changanford.common.router.path.ARouterHomePath
 import com.changanford.evos.databinding.ActivitySplashBinding
 import com.changanford.my.BaseMineUI
+
 
 @Route(path = ARouterHomePath.SplashActivity)
 class SplashActivity : BaseMineUI<ActivitySplashBinding, EmptyViewModel>() {
@@ -17,6 +20,14 @@ class SplashActivity : BaseMineUI<ActivitySplashBinding, EmptyViewModel>() {
 
     override fun initData() {
 //
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (intent.flags and Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT != 0) {
+            finish()
+            return
+        }
     }
 
     override fun isUseFullScreenMode(): Boolean {
