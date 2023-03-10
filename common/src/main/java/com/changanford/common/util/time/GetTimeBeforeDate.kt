944176d41bf2b0992b175d272e91ddcb6c19c103
牛啵啵 <>
2023-03-10 09:11:08 +0800

    时间工具类

---
+++ common/src/main/java/com/changanford/common/util/time/GetTimeBeforeDate.kt
@@ -0,0 +1,25 @@
+package com.changanford.common.util.time
+
+import android.annotation.SuppressLint
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ *Author lcw
+ *Time on 2023/3/9
+ *Purpose
+ */
+object GetTimeBeforeDate {
+
+    @SuppressLint("SimpleDateFormat")
+    fun getTimeDate(before: Int) {
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, -before)
+        val date = calendar.time
+        val simpleDateFormat = SimpleDateFormat("yyyyMMdd")
+
+        val stringDate: String = simpleDateFormat.format(date)
+
+    }
+}
